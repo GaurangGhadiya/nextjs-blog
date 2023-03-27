50107916f4e244d066efa187e271b318be3fe401
gaurang007 <gaurangghadiya99@gmail.com>
2023-03-27 23:01:31 +0530

    add try cstch

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -2,13 +2,18 @@ import fs from "fs"
 
 export default async function handler(req, res) {
   var response = [];
-  let files = await fs.promises.readdir("BlogData")
-  if(files.length){
-    for(let i = 0; i< files?.length; i++) {
-      let blog = await fs.promises.readFile(`BlogData/${files?.[i]}`,'utf-8')
-      response.push(JSON.parse(blog))
+  try{
+    let files = await fs.promises.readdir("BlogData")
+    if(files.length){
+      for(let i = 0; i< files?.length; i++) {
+        let blog = await fs.promises.readFile(`BlogData/${files?.[i]}`,'utf-8')
+        response.push(JSON.parse(blog))
+      }
     }
-  }
-  res.status(200).json({response })
+    res.status(200).json({response })
+  }catch(e){
+    res.status(200).json({response : "sonething went wrong" })
+}  
+
 }
   
