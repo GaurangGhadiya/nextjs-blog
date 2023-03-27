ea77b76e893c2106d3382da5ac4e0035c0aaaa39
gaurang007 <gaurangghadiya99@gmail.com>
2023-03-27 23:33:29 +0530

    Fix issue

--- src/pages/api/blogs.js
+++ src/pages/api/blogs.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     }
     res.status(200).json({response })
   }catch(e){
-    res.status(200).json({response : "sonething went wrong" })
+    res.status(200).json({response : e })
 }  
 
 }
